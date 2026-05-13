c3faee381e4b9b4904935fd3d975926d33287c11
Jonas Dreher <jonasdreher429@gmail.com>
2026-05-13 12:36:06 +0200

    Implemented DuengerCycle.cs

--- src/FruitBattlerWPF/Classes/DuengerCycle.cs
+++ src/FruitBattlerWPF/Classes/DuengerCycle.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace FruitBattlerWPF.Classes
 {
     class DuengerCycle
     {
+        private int CurrentStep;
+
+        public DuengerCycle()
+        {
+            CurrentStep = 0;
+        }
+
+        public int GetdNextValue()
+        {
+            CurrentStep++;
+            return CurrentStep;
+        }
+
+        public void Reset()
+        {
+            CurrentStep = 0;
+        }
     }
 }
