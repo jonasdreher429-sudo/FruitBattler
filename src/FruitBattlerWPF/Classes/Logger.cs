342bc8585d1029ddb874532cb0efccd92fbba650
sw4ggerduck <marko260775@icloud.com>
2026-05-20 12:29:39 +0200

    Added Logger Class and installed Serilog, Serilog.Sinks.Console and Serilog.Sinks.File to whole Project

---
+++ src/FruitBattlerWPF/Classes/Logger.cs
@@ -0,0 +1,54 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitBattlerWPF.Classes
+{
+    public static class Logger
+    {
+        static Logger()
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File(
+            "log/log-.txt",
+            rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: 7)
+            .CreateLogger();
+        }
+
+        public static void Information(string message)
+        {
+            Log.Information(message);
+        }
+
+        public static void Warning(string message)
+        {
+            Log.Warning(message);
+        }
+
+        public static void Error(string message)
+        {
+            Log.Error(message);
+        }
+
+        public static void Error(Exception ex, string message)
+        {
+            Log.Error(ex, message);
+        }
+
+        public static void Debug(string message)
+        {
+            Log.Debug(message);
+        }
+
+        public static void Close()
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
