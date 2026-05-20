aa91a6574651cc117571b715e878b8103b4cd73d
sw4ggerduck <marko260775@icloud.com>
2026-05-20 11:57:15 +0200

    Added Timetable

---
+++ src/FruitBattlerWPF/UserControls/MystikokosUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FruitBattlerWPF.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für MystikokosUserControl.xaml
+    /// </summary>
+    public partial class MystikokosUserControl : UserControl
+    {
+        public MystikokosUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
