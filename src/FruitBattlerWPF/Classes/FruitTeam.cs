be13f4f8661db166df6874e33db4d5e36f168ba1
Jonas Dreher <jonasdreher429@gmail.com>
2026-05-22 09:32:40 +0200

    Implemented FruitTeam

--- src/FruitBattlerWPF/Classes/FruitTeam.cs
+++ src/FruitBattlerWPF/Classes/FruitTeam.cs
@@ -8,5 +8,36 @@ namespace FruitBattlerWPF.Classes
 {
     class FruitTeam
     {
+        public Fruit[] Fruits { get; set; }
+        public int Active_Fruit_Index = 0;
+
+        public FruitTeam(Fruit[] Fruits)
+        {
+            this.Fruits = Fruits;
+        }
+
+        public Fruit GetActiveFruit()
+        {
+            return Fruits[Active_Fruit_Index];
+        }
+
+        public void SwitchFruit(int NewfruitIndex)
+        {
+            Active_Fruit_Index = NewfruitIndex;
+        }
+
+        public bool HasAliveFruit()
+        {
+            foreach (Fruit f in Fruits)
+            {
+                if (f.IsAlive)
+                    return true;
+            }
+            return false;
+        }
+
+
+
+
     }
 }
