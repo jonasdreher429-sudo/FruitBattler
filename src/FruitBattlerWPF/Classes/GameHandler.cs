d5e53eba93bf53be377e71942d234fd33eab5cc6
Jonas Dreher <jonasdreher429@gmail.com>
2026-05-26 15:28:15 +0200

    Wrote all methods for the GameHandler but have to prgramm EnemyKi first

--- src/FruitBattlerWPF/Classes/GameHandler.cs
+++ src/FruitBattlerWPF/Classes/GameHandler.cs
@@ -8,5 +8,64 @@ namespace FruitBattlerWPF.Classes
 {
     class GameHandler
     {
+        public int roundcount;
+        public int CurrentDungerPlayer;
+        public int CurrentDungerEnemy;
+        public FruitTeam CurrentPlayerTeam;
+        public FruitTeam CurrentEnemyTeam;
+        public bool IsGameOver;
+        public bool PlayerPLaying;
+       
+
+        public GameHandler()
+        {
+            roundcount = 0;
+            CurrentDungerPlayer = 0;
+            CurrentDungerEnemy = 0;
+            IsGameOver = false;
+        }
+
+        public GameHandler(FruitTeam PlayerTeam, FruitTeam EnemyTeam) : this()
+        {
+            CurrentPlayerTeam = PlayerTeam;
+            CurrentEnemyTeam = EnemyTeam;
+        }
+
+        public void StartGame()
+        {
+
+        }
+
+        public void NextRound()
+        {
+
+        }
+
+        public int CalculateDamage()
+        {
+
+        }
+
+        public void ApplyMove()
+        {
+
+        }
+
+        public void HandleFait()
+        {
+
+        }
+
+        public void SwitchFruit()
+        {
+
+        }
+
+        public int GetNextDuengerValue()
+        {
+
+        }
+
+
     }
 }
